4b916aee9518403986ba8cfdd6bc01cbd2c3289c
Mohit Bansal <bansalmohit9414@gmail.com>
2020-02-11 22:36:20 +0530

    Commit to Project

---
+++ src/javaapplication2/client.java
@@ -0,0 +1,343 @@
+package javaapplication2;
+
+import javax.swing.JOptionPane;
+import java.io.*;
+import java.net.*;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class client extends javax.swing.JFrame {
+    
+    public ObjectInputStream objectInputStream;
+    public ObjectOutputStream objectOutputStream;
+    public receiveMessage receive;
+    public filePicker filepicker;
+    public destinationPicker desPicker;
+    public static String Dir = "C:\\Users\\USER\\Desktop\\XYZ\\";
+    public boolean flag=false;
+    
+    public client() {
+        initComponents();
+        userName.setText(start.UserName);
+        textarea.setEditable(false);
+        filepicker = new filePicker();
+        desPicker = new destinationPicker();
+        try{
+        objectOutputStream = new ObjectOutputStream(start.socket.getOutputStream());
+        objectInputStream = new ObjectInputStream(start.socket.getInputStream());
+        System.out.println("Successfully Open Streams for "+ start.UserName);
+        }catch(IOException e)
+        {
+            System.out.println("Error in initializing streams"+e);
+        }
+        
+        receive = new receiveMessage(this);
+        receive.t.start();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        textarea = new javax.swing.JTextArea();
+        message = new javax.swing.JTextField();
+        send = new javax.swing.JButton();
+        sendfiles = new javax.swing.JButton();
+        close = new javax.swing.JButton();
+        userName = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        textarea.setColumns(20);
+        textarea.setRows(5);
+        jScrollPane1.setViewportView(textarea);
+
+        message.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                messageActionPerformed(evt);
+            }
+        });
+
+        send.setText("Send");
+        send.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendActionPerformed(evt);
+            }
+        });
+
+        sendfiles.setText("Send files");
+        sendfiles.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendfilesActionPerformed(evt);
+            }
+        });
+
+        close.setText("close");
+        close.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                closeActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(close, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(64, 64, 64))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(message, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(send)
+                                .addGap(31, 31, 31)
+                                .addComponent(sendfiles))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 563, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(248, 248, 248)
+                        .addComponent(userName, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(47, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addComponent(userName, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 302, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(close)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(message, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(send, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(sendfiles, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(46, 46, 46))))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void messageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_messageActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_messageActionPerformed
+
+    private void sendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendActionPerformed
+        // TODO add your handling code here:
+        String str = message.getText();
+        System.out.println("Sending Message");
+        try{
+        objectOutputStream.writeObject("0"+start.UserName+ ": " +str);
+        }catch(IOException e)
+        {
+            System.out.println("Error in Sending Message"+e);
+        }
+        print(start.UserName+ ": " +str);
+        message.setText("");
+    }//GEN-LAST:event_sendActionPerformed
+
+    private void closeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeActionPerformed
+        try {
+            // TODO add your handling code here:
+            flag=true;
+            objectInputStream.close();
+            objectOutputStream.close();
+            start.socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.dispose();
+    }//GEN-LAST:event_closeActionPerformed
+
+    private void sendfilesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendfilesActionPerformed
+        // TODO add your handling code here:
+        
+        String path = filepicker.getPath();
+        int count=0;
+        FileInputStream fileInputStream = null;
+        File f = new File(path);
+        
+        try {
+             fileInputStream = new FileInputStream(f);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        byte b[] = new byte[1000];
+        byte[] c;
+        System.out.println("Start Sending File");
+        try {
+            objectOutputStream.writeObject("1"+f.getName());
+            while((count=fileInputStream.read(b))!=-1){
+               c = Arrays.copyOf(b, count);
+            objectOutputStream.writeObject(c);
+            }
+            objectOutputStream.writeObject("EOF".getBytes());
+            System.out.println("End of file");
+        } catch (IOException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            fileInputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        print("Successfully Send File : "+f.getName());
+    }//GEN-LAST:event_sendfilesActionPerformed
+
+    public void receiveFile(String fileName)
+    {   
+        
+        File f = new File(Dir+fileName);
+        FileOutputStream fileOutputStream = null;
+        
+        try {
+            fileOutputStream = new FileOutputStream(f);
+        } catch (IOException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        byte b[] = new byte[1005];
+        String s;
+        while(true)
+        {
+            try {
+                b = (byte[])objectInputStream.readObject();
+                s = new String(b);
+                if(s.compareTo("EOF")==0){
+                   System.out.println("End of zfile");
+                    break;
+                }
+                else
+                fileOutputStream.write(b);
+               
+            } catch (IOException ex) {
+                Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+            
+        }
+        try {
+            fileOutputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+        System.out.println("Returning from Receive File");
+        print("Successfully Received File : "+fileName);
+    }
+    
+    synchronized public void print(String s)
+    {
+        String pre = textarea.getText();
+        pre = pre+ s + "\n";
+        textarea.setText(pre);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new client().setVisible(true);
+            }
+        });
+    }
+    
+    
+    class receiveMessage implements Runnable
+    {
+        Thread t;
+        client c;
+        String s;
+        public receiveMessage(client ch)
+        {
+            this.c = ch;
+            this.t = new Thread(this);
+        }
+        public void run()
+        {
+            while(true)
+            {
+                try{
+                s = (String)c.objectInputStream.readObject();
+                if(s.charAt(0)=='1'){
+                    System.out.println("Receiving File");
+                   c.receiveFile(s.substring(1,s.length()));
+                   continue;
+                }
+                System.out.println("Message Received");
+                c.print(s.substring(1,s.length()));
+                }catch(IOException e)
+                {
+                    System.out.println("Socket Error");
+                    if(!flag)
+                    {
+                        JOptionPane.showMessageDialog(c, "Your Friend is offline Now\nRedirecting to starting Page");
+                         try {
+                               Thread.sleep(500);
+                           } catch (InterruptedException ex) {
+                              Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+                         }
+                           new start().setVisible(true);
+                         c.dispose();
+                          break;
+                    }
+                    else
+                        break;
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton close;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField message;
+    private javax.swing.JButton send;
+    private javax.swing.JButton sendfiles;
+    private javax.swing.JTextArea textarea;
+    private javax.swing.JLabel userName;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/javaapplication2/filePicker.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+ import java.io.File;
+ import javax.swing.JFileChooser;
+ import javax.swing.filechooser.FileNameExtensionFilter;
+ import javax.swing.filechooser.FileSystemView;
+/**
+ *
+ * @author USER
+ */
+public class filePicker {
+    
+    String getPath()
+    {
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Choose A File");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG and GIF images", "png", "gif");
+	jfc.addChoosableFileFilter(filter);
+        jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int returnValue = jfc.showDialog(null, "Read");
+        //int returnValue = jfc.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = jfc.getSelectedFile();
+            return selectedFile.getAbsolutePath();
+        }
+        return "";
+    }
+}
